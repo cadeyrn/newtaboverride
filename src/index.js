fcb0eb36ef891f2cb4c7487e262f5432ddb1570b
Sören Hentzschel <kontakt@agenedia.com>
2015-11-21 20:03:02 +0100

    provide more options: about:blank, about:home, about:newtab and (first) homepage

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
-const {Cc, Ci} = require('chrome');
-const preferences = require('sdk/simple-prefs');
+const { Cc, Ci } = require('chrome');
+const { PrefsTarget } = require('sdk/preferences/event-target');
 const aboutNewTabService = Cc['@mozilla.org/browser/aboutnewtab-service;1'].getService(Ci.nsIAboutNewTabService);
+const simplePrefs = require('sdk/simple-prefs');
+const preferencesService = require('sdk/preferences/service');
+const prefsTarget = PrefsTarget({ branchName: 'browser.startup.'});
 
 const newtaboverride = {
   init : function () {
@@ -8,13 +11,34 @@ const newtaboverride = {
   },
   
   onPrefChange : function () {
-    aboutNewTabService.newTabURL = preferences.prefs['url'] || 'about:newtab';
+    var type = simplePrefs.prefs['type'];
+    var newTabUrl;
+
+    switch (type) {
+      case 'about:blank':
+      case 'about:home':
+      case 'about:newtab':
+        newTabUrl = type;
+        break;
+      case 'custom_url':
+        newTabUrl = simplePrefs.prefs['url'];
+        break;
+      case 'homepage':
+        var homepage = preferencesService.get('browser.startup.homepage', 'about:blank').split('|')[0];
+        newTabUrl = homepage;
+        break;
+      default:
+        newTabUrl = 'about:newtab';
+    }
+
+    aboutNewTabService.newTabURL = newTabUrl;
   }
 };
 
 const main = () => {
   newtaboverride.init();
-  preferences.on('url', newtaboverride.onPrefChange);
+  simplePrefs.on('', newtaboverride.onPrefChange);
+  prefsTarget.on('homepage', newtaboverride.onPrefChange);
 };
 
 exports.main = main;
