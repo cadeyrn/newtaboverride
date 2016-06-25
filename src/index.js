2d2b74c0a6f0a27c7b0c8a86db424b9446ab1478
Sören Hentzschel <kontakt@agenedia.com>
2016-06-25 14:32:51 +0200

    Neue Oberfläche für Einstellungen

--- src/index.js
+++ src/index.js
@@ -1,22 +1,28 @@
 const ONE_SECOND_IN_MILLISECONDS = 1000;
 const URL_CHARS_LIMIT = 2000;
 
+const _ = require('sdk/l10n').get;
+const { ActionButton } = require('sdk/ui/button/action');
 const { PrefsTarget } = require('sdk/preferences/event-target');
 const { setInterval, clearInterval } = require('sdk/timers');
 const clipboard = require('sdk/clipboard');
+const data = require('sdk/self').data;
 const newTabUrlJsm = require('resource:///modules/NewTabURL.jsm').NewTabURL;
 const preferencesService = require('sdk/preferences/service');
 const prefsTarget = PrefsTarget({ branchName: 'browser.startup.'});
 const simplePrefs = require('sdk/simple-prefs');
+const tabs = require('sdk/tabs');
 
 const newtaboverride = {
+  actionButton : null,
   lastClipboardUrl : false,
   timer : false,
 
   init : function () {
     newtaboverride.onPrefChange();
+    newtaboverride.createButton();
   },
-  
+
   onPrefChange : function () {
     var type = simplePrefs.prefs['type'];
     var newTabUrl;
@@ -54,6 +60,47 @@ const newtaboverride = {
     }
 
     newTabUrlJsm.override(newTabUrl);
+
+    for (let tab of tabs) {
+      if (tab.url === data.url('html/settings.html')) {
+        newtaboverride.syncPreferencesForOptionsUi(tab);
+        return;
+      }
+    }
+  },
+
+  createButton : function () {
+    newtaboverride.actionButton = ActionButton({
+      id : 'newtaboverride-button',
+      label : _('settings_title_short'),
+      icon : {
+        '18' : data.url('images/icon-18.png'),
+        '32' : data.url('images/icon-32.png'),
+        '36' : data.url('images/icon-36.png'),
+        '64' : data.url('images/icon-64.png')
+      },
+      onClick : () => {
+        if (newtaboverride.actionButton.badge) {
+          newtaboverride.actionButton.badge = null;
+        }
+
+        for (let tab of tabs) {
+          if (tab.url === data.url('html/settings.html')) {
+            tab.activate();
+            return;
+          }
+        }
+
+        tabs.open({
+          url : 'html/settings.html',
+          onReady : (tab) => {
+            newtaboverride.syncPreferencesForOptionsUi(tab).port.on('change-preference', (preference) => {
+              simplePrefs.prefs[preference.key] = preference.value;
+            });
+          }
+        });
+      }
+    });
   },
 
   clipboardAction : function () {
@@ -81,22 +128,37 @@ const newtaboverride = {
     var aboutpage = /^about:(about|accounts|addons|blank|buildconfig|cache|checkerboard|config|crashes|credits|debugging|downloads|healthreport|home|license|logo|memory|mozilla|networking|newtab|performance|plugins|preferences|privatebrowsing|profiles|rights|robots|searchreset|serviceworkers|support|sync-log|sync-tabs|telemetry|webrtc)?$/i;
 
     return website.test(string) || aboutpage.test(string);
+  },
+
+  syncPreferencesForOptionsUi : function (tab) {
+    const worker = tab.attach({
+      contentScriptFile : data.url('js/settings.js')
+    });
+
+    worker.port.emit('show-preferences', simplePrefs);
+
+    return worker;
   }
 };
 
-const main = () => {
+const main = (options) => {
   newtaboverride.init();
 
   simplePrefs.on('', newtaboverride.onPrefChange);
   prefsTarget.on('homepage', newtaboverride.onPrefChange);
+
+  if (options.loadReason === 'install') {
+    newtaboverride.actionButton.badge = '★';
+  }
 };
 
-exports.main = main;
-
-exports.onUnload = function (reason) {
+const unload = (reason) => {
   if (reason === 'uninstall' || reason === 'disable') {
     clearInterval(newtaboverride.timer);
     newtaboverride.lastClipboardUrl = false;
     newTabUrlJsm.reset();
   }
 };
+
+exports.main = main;
+exports.onUnload = unload;
