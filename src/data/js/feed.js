bcf355d677e8551de5b240bd8c8ab1e1db2be0dc
Sören Hentzschel <kontakt@agenedia.com>
2016-10-06 01:36:09 +0200

    HTML-Fehler korrigiert

--- src/data/js/feed.js
+++ src/data/js/feed.js
@@ -6,9 +6,9 @@ self.port.on('feed-items', (items) => {
     let dateAsString = date.getDate() + '.' + (date.getMonth() + 1) + '.' + date.getFullYear();
 
     let el = document.createElement('li');
-    el.innerHTML = '<li><small>' + window.global_i18n.feed_published_at + ' ' + dateAsString + '</small><br />' +
+    el.innerHTML = '<small>' + window.global_i18n.feed_published_at + ' ' + dateAsString + '</small><br />' +
       '<a href="' + items[i].link + '"><strong>' + items[i].title + '</strong></a><br />' + items[i].description +
-      '<a href="' + items[i].link + '" class="button">' + window.global_i18n.feed_read_more + '</a></li>';
+      '<a href="' + items[i].link + '" class="button">' + window.global_i18n.feed_read_more + '</a>';
     document.getElementById('feed-items').appendChild(el);
   }
 });
