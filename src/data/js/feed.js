ddd031a994c79a69d8d28a1c416ad96df0aa44bd
Sören Hentzschel <kontakt@agenedia.com>
2016-10-06 01:53:47 +0200

    Feed-Links in neuen Tabs öffnen

--- src/data/js/feed.js
+++ src/data/js/feed.js
@@ -7,8 +7,9 @@ self.port.on('feed-items', (items) => {
 
     let el = document.createElement('li');
     el.innerHTML = '<small>' + window.global_i18n.feed_published_at + ' ' + dateAsString + '</small><br />' +
-      '<a href="' + items[i].link + '"><strong>' + items[i].title + '</strong></a><br />' + items[i].description +
-      '<a href="' + items[i].link + '" class="button">' + window.global_i18n.feed_read_more + '</a>';
+      '<a href="' + items[i].link + '" target="_blank"><strong>' + items[i].title + '</strong></a><br />' +
+      items[i].description + '<a href="' + items[i].link + '" class="button" target="_blank">' +
+      window.global_i18n.feed_read_more + '</a>';
     document.getElementById('feed-items').appendChild(el);
   }
 });
