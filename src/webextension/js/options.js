cafc5740540bad37ae549b29bb8b9d55d72edec7
Sören Hentzschel <kontakt@agenedia.com>
2017-01-25 20:17:29 +0100

    options.js modernisiert

--- src/webextension/js/options.js
+++ src/webextension/js/options.js
@@ -1,8 +1,10 @@
-const elType = document.querySelector('#type');
-const elUrlOption = document.querySelector('#url_option');
-const elUrl = document.querySelector('#url');
-const elFocusWebsite = document.querySelector('#focus_website');
-const elClearLocationBar = document.querySelector('#clear_locationbar');
+'use strict';
+
+const elType = document.getElementById('type');
+const elUrlOption = document.getElementById('url_option');
+const elUrl = document.getElementById('url');
+const elFocusWebsite = document.getElementById('focus_website');
+const elClearLocationBar = document.getElementById('clear_locationbar');
 
 const options = {
   load : function () {
@@ -10,57 +12,40 @@ const options = {
       elUrlOption.style.display = (elType.options[elType.selectedIndex].value == 'custom_url') ? 'block' : 'none';
     }
 
-    function setType(result) {
-      const value = result.type || 'custom_url';
-      elType.querySelector('[value="' + value + '"]').selected = true;
+    browser.storage.local.get({ 'type' : 'custom_url' }).then((option) => {
+      elType.querySelector('[value="' + option.type + '"]').selected = true;
       toggleUrlOption();
-    }
+    });
 
-    function setUrl(result) {
-      elUrl.value = result.url || 'about:newtab';
-    }
+    browser.storage.local.get({ 'url' : 'about:newtab' }).then((option) => {
+      elUrl.value = option.url;
+    });
 
-    function setFocusWebsite(result) {
-      elFocusWebsite.checked = result.focus_website || false;
-    }
+    browser.storage.local.get({ 'focus_website' : false }).then((option) => {
+      elFocusWebsite.checked = option.focus_website;
+    });
 
-    function setClearLocationBar(result) {
-      elClearLocationBar.checked = result.clear_locationbar || false;
-    }
-
-    function onError(error) {
-      console.log(`Error: ${error}`);
-    }
-
-    let type = browser.storage.local.get('type');
-    type.then(setType, onError);
-
-    let url = browser.storage.local.get('url');
-    url.then(setUrl, onError);
-
-    let focus_website = browser.storage.local.get('focus_website');
-    focus_website.then(setFocusWebsite, onError);
-
-    let clear_locationbar = browser.storage.local.get('clear_locationbar');
-    clear_locationbar.then(setClearLocationBar, onError);
+    browser.storage.local.get({ 'clear_locationbar' : false }).then((option) => {
+      elClearLocationBar.checked = option.clear_locationbar;
+    });
   }
 };
 
 document.addEventListener('DOMContentLoaded', options.load);
 
 elType.addEventListener('change', function () {
-  browser.storage.local.set({'type' : this.value});
+  browser.storage.local.set({ 'type' : this.value });
   toggleUrlOption();
 });
 
 elUrl.addEventListener('input', function () {
-  browser.storage.local.set({'url' : this.value});
+  browser.storage.local.set({ 'url' : this.value });
 });
 
 elFocusWebsite.addEventListener('change', function () {
-  browser.storage.local.set({'focus_website' : this.checked});
+  browser.storage.local.set({ 'focus_website' : this.checked });
 });
 
 elClearLocationBar.addEventListener('change', function () {
-  browser.storage.local.set({'clear_locationbar' : this.checked});
+  browser.storage.local.set({ 'clear_locationbar' : this.checked });
 });
