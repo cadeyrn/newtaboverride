f0714c64aeaa6a31bb56c030b3a81146fe816580
Sören Hentzschel <kontakt@agenedia.com>
2017-07-29 11:20:04 +0200

    implemented omnibox command for opening the options page

--- src/js/background.js
+++ src/js/background.js
@@ -22,6 +22,56 @@ const newtaboverride = {
   },
 
   /**
+   * Fired whenever the user changes the input, after the user has started interacting with the add-on by entering
+   * its keyword in the address bar and then pressing the space key.
+   *
+   * @param {string} input - user input in the address bar, not including the add-on's keyword itself or the space
+   *                 after the keyword<br /><br />
+   *                 <strong>Supported value:</strong> settings
+   * @param {function} suggest - a callback function that the event listener can call to supply suggestions for the
+   *                   address bar's drop-down list
+   *
+   * @returns {void}
+   */
+  showOmniboxSuggestions (input, suggest) {
+    const availableCommands = ['settings'];
+    const suggestions = [];
+
+    for (const command of availableCommands) {
+      if (command.indexOf(input) !== -1) {
+        suggestions.push({
+          content : command,
+          description : browser.i18n.getMessage('omnibox_command_' + command)
+        });
+      }
+
+      if (suggestions.length === 0) {
+        suggestions.push({
+          content : 'settings',
+          description : browser.i18n.getMessage('omnibox_command_settings')
+        });
+      }
+    }
+
+    suggest(suggestions);
+  },
+
+  /**
+   * Fired when the user has selected one of the suggestions the add-on has added to the address bar's drop-down list.
+   *
+   * @param {string} input - this is the value that the user selected
+   *
+   * @returns {void}
+   */
+  callOmniboxAction (input) {
+    switch (input) {
+      case 'settings':
+      default:
+        newtaboverride.openUserInterfaceInCurrentTab();
+    }
+  },
+
+  /**
    * Fired when the toolbar icon is clicked. This method is used to open the user interface in a new tab or to switch
    * to the tab with the user interface if the user interface is already opened.
    *
@@ -48,8 +98,20 @@ const newtaboverride = {
         browser.tabs.create({ url });
       }
     });
+  },
+
+  /**
+   * This method is used to open the user interface in the current tab. It's used for the omnibox suggestions.
+   *
+   * @returns {void}
+   */
+  openUserInterfaceInCurrentTab () {
+    browser.tabs.update(null, { url : browser.extension.getURL(OPTIONS_PAGE) });
   }
 };
 
 browser.browserAction.onClicked.addListener(newtaboverride.openUserInterface);
+browser.omnibox.onInputChanged.addListener(newtaboverride.showOmniboxSuggestions);
+browser.omnibox.onInputEntered.addListener(newtaboverride.callOmniboxAction);
+browser.omnibox.setDefaultSuggestion({ description : browser.i18n.getMessage('omnibox_default_description') });
 browser.runtime.onInstalled.addListener(newtaboverride.onInstalledHandler);
