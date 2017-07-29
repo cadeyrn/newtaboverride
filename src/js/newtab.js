7da5892c11d875cd81a026ea04a491ac55376256
Sören Hentzschel <kontakt@agenedia.com>
2017-07-29 22:21:04 +0200

    added validation for custom url option because only valid http(s) urls are allowed. It's better to handle these URLs as about:blank than to fail to load these URLs

--- src/js/newtab.js
+++ src/js/newtab.js
@@ -3,6 +3,11 @@
 const FEED_PAGE = 'html/feed.html';
 
 /**
+ * @see http://stackoverflow.com/a/9284473
+ */
+const URI_REGEX = /^(?:(?:https?):\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,}))\.?)(?::\d{2,5})?(?:[/?#]\S*)?$/i;
+
+/**
  * @exports newtab
  */
 const newtab = {
@@ -19,15 +24,19 @@ const newtab = {
           browser.tabs.update({ url : options.type });
           break;
         case 'custom_url':
-          browser.tabs.update({ url : options.url || 'about:blank' });
+          browser.tabs.update({ url : newtab.isValidUri(options.url) ? options.url : 'about:blank' });
           break;
         case 'feed':
           browser.tabs.update({ url : browser.extension.getURL(FEED_PAGE) });
           break;
         default:
-          // do nothing
+          browser.tabs.update({ url : 'about:blank' });
       }
     });
+  },
+
+  isValidUri : function (string) {
+    return URI_REGEX.test(string);
   }
 };
 
