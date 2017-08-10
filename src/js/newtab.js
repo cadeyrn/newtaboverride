7fbbf96b7bbac85b63b006959cac8f080b971750
Sören Hentzschel <kontakt@agenedia.com>
2017-08-10 13:10:48 +0200

    validate URL on change, no longer on every tab opening

--- src/js/newtab.js
+++ src/js/newtab.js
@@ -3,11 +3,6 @@
 const FEED_PAGE = 'html/feed.html';
 
 /**
- * @see http://stackoverflow.com/a/9284473
- */
-const URI_REGEX = /^(?:(?:https?):\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,}))\.?)(?::\d{2,5})?(?:[/?#]\S*)?$/i;
-
-/**
  * @exports newtab
  */
 const newtab = {
@@ -24,18 +19,17 @@ const newtab = {
           browser.tabs.update({ url : options.type });
           break;
         case 'custom_url':
-          const newTabUrl = newtab.isValidUri(options.url) ? options.url : 'about:blank';
           if (options.focus_website) {
             browser.tabs.getCurrent((tab) => {
               const tabId = tab.id;
-              const created = browser.tabs.create({ 'url' : newTabUrl });
+              const created = browser.tabs.create({ 'url' : options.url });
               created.then(() => {
                 browser.tabs.remove(tabId);
               });
             });
           }
           else {
-            browser.tabs.update({ url : newTabUrl });
+            browser.tabs.update({ url : options.url });
           }
           break;
         case 'feed':
@@ -45,10 +39,6 @@ const newtab = {
           browser.tabs.update({ url : 'about:blank' });
       }
     });
-  },
-
-  isValidUri (string) {
-    return URI_REGEX.test(string);
   }
 };
 
--- src/js/options.js
+++ src/js/options.js
@@ -2,6 +2,11 @@
 
 const FEED_PERMISSION = { origins : ['https://www.soeren-hentzschel.at/*'] };
 
+/**
+ * @see http://stackoverflow.com/a/9284473
+ */
+const URI_REGEX = /^(?:(?:https?):\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,}))\.?)(?::\d{2,5})?(?:[/?#]\S*)?$/i;
+
 const elCompatNotice = document.getElementById('compat-notice');
 const elDefaultOption = document.getElementById('default_option');
 const elFeedPermission = document.getElementById('feed_permission_container');
@@ -17,6 +22,10 @@ const elUrl = document.getElementById('url');
  * @exports options
  */
 const options = {
+  isValidUri (string) {
+    return URI_REGEX.test(string);
+  },
+
   toggleOptionsDetails () {
     if (elType.options[elType.selectedIndex].value === 'default') {
       elDefaultOption.classList.remove('hidden');
@@ -112,5 +121,10 @@ elType.addEventListener('change', (e) => {
 });
 
 elUrl.addEventListener('input', (e) => {
-  browser.storage.local.set({ url : e.target.value });
+  if (options.isValidUri(e.target.value)) {
+    browser.storage.local.set({ url : e.target.value });
+  }
+  else {
+    browser.storage.local.set({ url : 'about:blank' });
+  }
 });
