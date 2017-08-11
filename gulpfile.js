aba56a69d68e54088e8741a532e88b1ddfb9b612
Sören Hentzschel <kontakt@agenedia.com>
2017-08-11 10:05:10 +0200

    added stylelint to devDependencies

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,21 @@
 
 const gulp = require('gulp');
 const gulpEslint = require('gulp-eslint');
+const gulpStylelint = require('gulp-stylelint');
 
 gulp.task('lint-js', () => gulp.src(['gulpfile.js', './src/js/*.js'])
   .pipe(gulpEslint({ configFile : '.eslintrc.json' }))
   .pipe(gulpEslint.format())
+);
+
+gulp.task('lint-css', () => gulp.src(['./src/css/*.css'])
+  .pipe(gulpStylelint({
+    failAfterError : false,
+    reporters : [
+      {
+        formatter : 'string',
+        console : true
+      }
+    ]
+  }))
 );
