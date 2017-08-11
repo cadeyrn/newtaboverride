c86696c27dc7f351515006cb503168f0e2021795
Sören Hentzschel <kontakt@agenedia.com>
2017-08-11 09:50:04 +0200

    fixed eslint issues in newtab.js

--- src/js/newtab.js
+++ src/js/newtab.js
@@ -1,5 +1,7 @@
 'use strict';
 
+/* global defaults */
+
 const FEED_PAGE = 'html/feed.html';
 
 /**
@@ -11,34 +13,33 @@ const newtab = {
    *
    * @returns {void}
    */
-  init () {
-    browser.storage.local.get(defaults, options => {
-      switch (options.type) {
-        case 'about:blank':
-        case 'about:home':
-          browser.tabs.update({ url : options.type });
-          break;
-        case 'custom_url':
-          if (options.focus_website) {
-            browser.tabs.getCurrent((tab) => {
-              const tabId = tab.id;
-              const created = browser.tabs.create({ 'url' : options.url || 'about:blank' });
-              created.then(() => {
-                browser.tabs.remove(tabId);
-              });
+  async init () {
+    const options = await browser.storage.local.get(defaults);
+
+    switch (options.type) {
+      case 'about:blank':
+      case 'about:home':
+        browser.tabs.update({ url : options.type });
+        break;
+      case 'custom_url':
+        if (options.focus_website) {
+          browser.tabs.getCurrent((tab) => {
+            const tabId = tab.id;
+            browser.tabs.create({ url : options.url || 'about:blank' }, () => {
+              browser.tabs.remove(tabId);
             });
-          }
-          else {
-            browser.tabs.update({ url : options.url || 'about:blank' });
-          }
-          break;
-        case 'feed':
-          browser.tabs.update({ url : browser.extension.getURL(FEED_PAGE) });
-          break;
-        default:
-          browser.tabs.update({ url : 'about:blank' });
-      }
-    });
+          });
+        }
+        else {
+          browser.tabs.update({ url : options.url || 'about:blank' });
+        }
+        break;
+      case 'feed':
+        browser.tabs.update({ url : browser.extension.getURL(FEED_PAGE) });
+        break;
+      default:
+        browser.tabs.update({ url : 'about:blank' });
+    }
   }
 };
 
