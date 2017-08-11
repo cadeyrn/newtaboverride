564a5deea3473b3b9ba0e2a460eb2020d4fcfe03
Sören Hentzschel <kontakt@agenedia.com>
2017-08-11 09:43:48 +0200

    fixed eslint issues in feed.js

--- src/js/feed.js
+++ src/js/feed.js
@@ -1,5 +1,7 @@
 'use strict';
 
+/* global feedreader */
+
 const FEED_PERMISSION = { origins : ['https://www.soeren-hentzschel.at/*'] };
 const FEED_URL = 'https://www.soeren-hentzschel.at/feed/';
 
@@ -33,8 +35,8 @@ const feed = {
     document.getElementById('throbber').remove();
 
     for (let i = 0; i < items.length; i++) {
-      let date = new Date(items[i].pubDate);
-      let dateAsString = date.getDate() + '.' + (date.getMonth() + 1) + '.' + date.getFullYear();
+      const date = new Date(items[i].pubDate);
+      const dateAsString = date.getDate() + '.' + (date.getMonth() + 1) + '.' + date.getFullYear();
 
       // strip html from description
       let description = items[i].description.replace(/<(?:.|\n)*?>/gm, '');
@@ -42,46 +44,46 @@ const feed = {
       // remove last paragraph from soeren-hentzschel.at feed
       description = description.replace(/(.*)\s*Der Beitrag.* erschien zuerst auf.*/, '$1');
 
-      let docFragment = document.createDocumentFragment();
+      const docFragment = document.createDocumentFragment();
 
-      let li = document.createElement('li');
+      const li = document.createElement('li');
       docFragment.appendChild(li);
 
-      let small = document.createElement('small');
+      const small = document.createElement('small');
       li.appendChild(small);
-      let text1 = document.createTextNode(browser.i18n.getMessage('feed_published_at') + ' ' + dateAsString);
+      const text1 = document.createTextNode(browser.i18n.getMessage('feed_published_at') + ' ' + dateAsString);
       small.appendChild(text1);
 
-      let br1 = document.createElement('br');
+      const br1 = document.createElement('br');
       li.appendChild(br1);
 
-      let a1 = document.createElement('a');
+      const a1 = document.createElement('a');
       a1.setAttribute('href', items[i].link);
       a1.setAttribute('target', '_blank');
       a1.setAttribute('rel', 'noopener');
       li.appendChild(a1);
 
-      let strong = document.createElement('strong');
+      const strong = document.createElement('strong');
       a1.appendChild(strong);
-      let text2 = document.createTextNode(items[i].title);
+      const text2 = document.createTextNode(items[i].title);
       strong.appendChild(text2);
 
-      let br2 = document.createElement('br');
+      const br2 = document.createElement('br');
       li.appendChild(br2);
 
-      let p = document.createElement('p');
+      const p = document.createElement('p');
       li.appendChild(p);
-      let text3 = document.createTextNode(description);
+      const text3 = document.createTextNode(description);
       p.appendChild(text3);
 
-      let a2 = document.createElement('a');
+      const a2 = document.createElement('a');
       a2.setAttribute('href', items[i].link);
       a2.setAttribute('class', 'button readmore-button');
       a2.setAttribute('target', '_blank');
       a2.setAttribute('rel', 'noopener');
       li.appendChild(a2);
 
-      let text4 = document.createTextNode(browser.i18n.getMessage('feed_read_more'));
+      const text4 = document.createTextNode(browser.i18n.getMessage('feed_read_more'));
       a2.appendChild(text4);
 
       document.getElementById('feed-items').appendChild(docFragment);
