f859d01b0b9923cfb0ecdf86ccb4ff68aee23aad
Sören Hentzschel <kontakt@agenedia.com>
2017-08-12 15:11:57 +0200

    use a much less restrictive url validation, fixes #5

--- src/js/options.js
+++ src/js/options.js
@@ -4,11 +4,8 @@
 
 const FEED_PERMISSION = { origins : ['https://www.soeren-hentzschel.at/*'] };
 
-/**
- * @see http://stackoverflow.com/a/9284473
- */
-// eslint-disable-next-line max-len
-const URI_REGEX = /^(?:(?:https?):\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,}))\.?)(?::\d{2,5})?(?:[/?#]\S*)?$/i;
+// a not very advanved regex to match most URLs…
+const URI_REGEX = /^https?:\/\/(.*)/i;
 
 const elCompatNotice = document.getElementById('compat-notice');
 const elDefaultOption = document.getElementById('default-option');
