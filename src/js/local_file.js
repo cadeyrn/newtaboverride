3847ac6b3b3e03cf8ca5eb6d70eccef512aa5812
Sören Hentzschel <kontakt@agenedia.com>
2017-08-20 13:40:06 +0200

    fixed ESLint issue

--- src/js/local_file.js
+++ src/js/local_file.js
@@ -8,7 +8,7 @@
 const localfile = {
   async init () {
     const body = document.querySelector('body');
-    const { local_file }  = await browser.storage.local.get({ local_file : defaults.local_file });
+    const { local_file } = await browser.storage.local.get({ local_file : defaults.local_file });
 
     // The file has been uploaded by the user. Therefore, the user wants the content, regardless of possible problems
     // eslint-disable-next-line no-unsanitized/method
