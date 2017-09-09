5209facc17d78424e1568a95673347a56d34aa13
Sören Hentzschel <kontakt@agenedia.com>
2017-09-09 19:49:34 +0200

    avoid console warning

--- src/js/core/utils.js
+++ src/js/core/utils.js
@@ -16,9 +16,9 @@ const utils = {
    */
   parseVersion (version) {
     const versionParts = version.split('.');
-    const major = parseInt(versionParts[0]) || 0;
-    const minor = parseInt(versionParts[1]) || 0;
-    const patch = parseInt(versionParts[2]) || 0;
+    const major = versionParts[0] ? parseInt(versionParts[0]) : 0;
+    const minor = versionParts[1] ? parseInt(versionParts[1]) : 0;
+    const patch = versionParts[2] ? parseInt(versionParts[2]) : 0;
 
     return { major, minor, patch };
   }
