a25f84cb000cb6dba05f8babc65ba6a35d878733
Sören Hentzschel <kontakt@agenedia.com>
2019-06-30 15:44:46 +0200

    supressing one eslint warning

--- src/js/features/local_file.js
+++ src/js/features/local_file.js
@@ -17,6 +17,7 @@ const localfile = {
     // set page title if there is a title tag in the document
     const match = local_file.match(/<title[^>]*>([^<]+)<\/title>/i);
     if (match) {
+      // eslint-disable-next-line require-atomic-updates
       document.title = match[1];
     }
   }
